4f9bb3de069393c124371d47699cc3c445ecd8df
pavlvs <pavlvsx@gmail.com>
2018-01-25 17:54:12 +0100

    Finish all REST apis

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
 const http = new easyHTTP();
 
-http.get("https://jsonplaceholder.typicode.com/posts",
-    function (err, posts) {
+// http.get('https://jsonplaceholder.typicode.com/posts',
+//     function (err, posts) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(posts);
+//         }
+//     });
+
+// http.get('https://jsonplaceholder.typicode.com/posts/1',
+//     function (err, post) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(post);
+//         }
+//     });
+
+// create the data
+
+const data = {
+    title: 'Custom foo',
+    body: ' This is a quux baz'
+};
+
+// create post
+
+// http.post('https://jsonplaceholder.typicode.com/posts',
+//     data, function (err, post) {
+
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(post);
+//         }
+//     });
+
+// Update post
+http.put("https://jsonplaceholder.typicode.com/posts/1", data,
+    function (err, post) { 
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(post);
+        }
+    });
+
+// Delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/1',
+    function (err, response) {
         if (err) {
             console.log(err);
         } else {
-            console.log(posts);
+            console.log(response);
         }
     });
